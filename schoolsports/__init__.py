6b0da89fef18ab62a64175dea54f4c443d0dece0
Anish <anish.kamath2005>
2021-06-19 21:39:10 +0530

    a

--- schoolsports/__init__.py
+++ schoolsports/__init__.py
@@ -46,7 +46,7 @@ from flask_migrate import Migrate
 migrate=Migrate(app,db, render_as_batch=True)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
 app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 mail=Mail(app)
